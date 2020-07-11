313bc8c3cad7501a80d341aea80d9aca5168d30b
ashrakrahman <lipu@mpower-social.com>
2020-07-11 20:36:54 +0600

    Updates

---
+++ precache-manifest.a4a5d58b518b72e9306b1ab88ad23fec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62bee8158a8cff7637db38b37421080a",
+    "url": "/premier-league-test/index.html"
+  },
+  {
+    "revision": "f9a05008f6f0a7d36f01",
+    "url": "/premier-league-test/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "31f0dc1f6a799af4dae5",
+    "url": "/premier-league-test/static/css/main.a865722b.chunk.css"
+  },
+  {
+    "revision": "f9a05008f6f0a7d36f01",
+    "url": "/premier-league-test/static/js/2.76b90db6.chunk.js"
+  },
+  {
+    "revision": "b5d23eb95f12f07608aa002de8d8de17",
+    "url": "/premier-league-test/static/js/2.76b90db6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31f0dc1f6a799af4dae5",
+    "url": "/premier-league-test/static/js/main.7050ea59.chunk.js"
+  },
+  {
+    "revision": "8121d85bfe92d5b3a15c",
+    "url": "/premier-league-test/static/js/runtime-main.4279b8e5.js"
+  }
+]);
